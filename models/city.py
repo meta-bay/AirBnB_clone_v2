c24900447a8676d77383c1ec22089a9fa164b0d1
meta-bay <bayiemeseret@gmail.com>
2024-02-22 03:53:50 +0300

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey, String
+from models.base_model import BaseModel, Base
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """
+    contains the table name, the name and the state id
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
