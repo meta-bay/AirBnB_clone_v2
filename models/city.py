d8689ef2cd7372adfe8628acb881286266c8947a
meta-bay <bayiemeseret@gmail.com>
2024-02-22 06:00:20 +0300

    nothing

--- models/city.py
+++ models/city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This is the city class"""
+"""city class"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
@@ -10,9 +10,6 @@ from models.place import Place
 
 class City(BaseModel, Base):
     """This is the class for City
-    Attributes:
-        state_id: The state id
-        name: input name
     """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
