3566b1c19927ef8b3f781fbf8b70a701407a2dc0
meta-bay <bayiemeseret@gmail.com>
2024-03-23 16:52:21 +0300

    just work

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,4 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     ''' hello function '''
+    strict_slashes=False
     return "Hello HBNB!"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
