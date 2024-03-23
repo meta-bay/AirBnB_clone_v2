a8c4181fa2fc29d19a678b53ae0ae3bea6087602
meta-bay <bayiemeseret@gmail.com>
2024-03-23 17:02:36 +0300

    hbnb

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 '''
-    starts a Flask web application
+	starts a Flask web application
 '''
-from flask import Flask, request
+from flask import Flask
 
 
 app = Flask(__name__)
@@ -10,10 +10,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    ''' hello function '''
+    ''' displays Hello HBNB!'''
     strict_slashes = False
     return "Hello HBNB!"
 
+@app.route('/hbnb')
+def hbnb():
+	''' displays HBNB '''
+	return "HBNB"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
