92039a0bae1ce77e1e8d48e0ddf7962456d083c7
meta-bay <bayiemeseret@gmail.com>
2024-03-24 00:41:52 +0300

    pycodestyle

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -40,13 +40,13 @@ def is_int(n):
 def is_int_html(n):
     return render_template('5-number.html', n=n)
 
+
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def odd_even(n):
     odven = 'odd'
     if n % 2 == 0:
         odven = 'even'
     return render_template('6-number_odd_or_even.html', n=n, odven=odven)
-
 
 
 if __name__ == "__main__":
