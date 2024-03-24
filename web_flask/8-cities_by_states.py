81dd769b8b6f16cee28d750662877f14574ef988
meta-bay <bayiemeseret@gmail.com>
2024-03-24 16:49:22 +0300

    cities by states

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
 '''
-    list of states
+    Cities by states
 '''
+from models.state import State
 from flask import Flask, render_template
 from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
+@app.route('/cities_by_states')
 def states_list():
     states = storage.all(State)
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template('7-states_list.html', sorted_states=sorted_states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
@@ -22,5 +21,6 @@ def app_teardown(arg=None):
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
